d3901582d1524044d98e811fd8df18ae4b1c7f7d
Elemental X <49472311+xelemental@users.noreply.github.com>
2021-01-31 20:08:57 +0530

    Added source code

---
+++ V-Lang/Hash/keyjoin.v
@@ -0,0 +1,49 @@
+fn main(){
+
+a := 23
+b := 0x98C
+
+if a == 45{
+
+	one()
+}else{
+	println('Oops wrong path')
+}
+
+if b == 76{
+	two()
+}else{
+	println('Oops wrong path')
+}
+}
+
+fn one(){
+	mut nums := []int{len :6, init: -2}
+	c := 45
+	if c < 32{
+			mut numstwo:= []int{cap :10}
+			for i in 2 .. 12{
+				numstwo<< i
+			 }
+			println(numstwo[2..8])
+		}else{
+			falsepositive := []int{len : 6, init: -3}
+			println("You don't have the first part of key yet")
+		}
+
+}
+fn two(){
+	mut character := []int{len :7, init: -7}
+	d := 90
+	if d < 45{
+			mut numsthree:= []string{cap :10}
+				mut k := [ 'J', 'K', 'L' , 'q', '5']
+				h := 77
+					if h < 76{
+							k << ['9' , 'U', '1', '3','3','7']
+							println(k)
+						}else{
+							println('You dont have the entire key yet.... :(')
+						    }
+	        }
+}
